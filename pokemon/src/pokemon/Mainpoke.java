5d597780387c06468a5028098bfdc6ae13d690c8
bluewt129 <bluewt129@gmail.com>
2024-04-17 10:44:44 +0900

    git commit -m "main 구동페이지"

---
+++ pokemon/src/pokemon/Mainpoke.java
@@ -0,0 +1,119 @@
+package pokemon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Mainpoke {
+	
+	static Login Login = new Login();
+	static Monster Monster = new Monster();
+	static AiBot AiBot = new AiBot();
+
+	public static void main(String[] args) throws InterruptedException {
+		ArrayList<User> users = new ArrayList<>();
+		ArrayList<String> my_mons = new ArrayList<>();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("포켓몬 세계에 오신 것을 환영합니다");
+		Thread.sleep(1000);
+		User logined = null;
+		Login login = new Login();
+		
+		while(true) {
+			if(logined == null) {
+				System.out.println("계정을 보유하고 계신가요? (1:예/2:아니오) ");
+				int rsp = sc.nextInt();
+				switch(rsp) {
+					case 1 :
+						logined = Login.loginSuccess(users);
+						if(logined == null) {
+							System.out.println("입력하신 정보가 존재하지 않습니다");
+						}
+						break;
+					case 2 : 
+						System.out.println("회원가입을 시작합니다");
+						login.addAccount(users);
+						System.out.println(users);
+						break;
+					default : System.out.println("입력 오류");
+				}
+			}else {
+				System.out.println(logined.getMyName()+"! 반갑네.");
+				Thread.sleep(1000);
+				System.out.println("내 이름은 오박사. 포켓몬 트레이너들의 모험을 도와주고 있지.");
+				Thread.sleep(1000);
+				System.out.println("어라? "+logined.getMyName()+". 자네는 포켓몬이 한 마리도 없구만.");
+				Thread.sleep(1000);
+				System.out.println("성공적인 모험을 위해 내가 한 마리 보내주겠네. 이 중에서 골라보게나.");
+				while(true) {
+					System.out.println("1.파이리 2.꼬부기 3.이상해씨");
+					int rspn = sc.nextInt();
+					if(rspn==1) {
+						System.out.println("파이리는 불꽃타입 포켓몬이지. 활발한 성격에 강한 공격력을 가지고 있단다.");
+						System.out.println("정말 파이리로 하겠니? (1:예/2:다시 고를래요)");
+						int rspn2 = sc.nextInt();
+						if(rspn2 == 1) {
+							System.out.println("훌륭한 선택이네. 여기, 파이리를 데려가게.");
+							Thread.sleep(1000);
+							System.out.println("파이리 : 파이리~!(주인 반가워~!)");
+							Thread.sleep(1000);
+							System.out.println("파이리가 모험에 참여했다!");
+							my_mons.add("파이리");
+							break;
+						}else if(rspn2==2){
+							System.out.println("신중하게 선택해보렴.");
+						}
+					}
+					if(rspn==2) {
+						System.out.println("꼬부기는 물타입 포켓몬이지. 차분한 성격에 강한 방어력을 가지고 있단다.");
+						System.out.println("정말 꼬부기로 하겠니? (1:예/2:다시 고를래요)");
+						int rspn2 = sc.nextInt();
+						if(rspn2 == 1) {
+							System.out.println("훌륭한 선택이네. 여기, 꼬부기를 데려가게.");
+							Thread.sleep(1000);
+							System.out.println("꼬부기 : 꼬부기~!(반가워요 주인님~!)");
+							Thread.sleep(1000);
+							System.out.println("꼬부기가 모험에 참여했다!");
+							my_mons.add("꼬부기");
+							break;
+						}else if(rspn2==2){
+							System.out.println("신중하게 선택해보렴.");
+						}
+					}
+					if(rspn==3) {
+						System.out.println("이상해씨는 풀타입 포켓몬이지. 과묵한 성격에 높은 체력을 가지고 있단다.");
+						System.out.println("정말 이상해씨로 하겠니? (1:예/2:다시 고를래요)");
+						int rspn2 = sc.nextInt();
+						if(rspn2 == 1) {
+							System.out.println("훌륭한 선택이네. 여기, 이상해씨를 데려가게.");
+							Thread.sleep(1000);
+							System.out.println("이상해씨 : 씨...(씨...)");
+							Thread.sleep(1000);
+							System.out.println("이상해씨가 모험에 참여했다!");
+							my_mons.add("이상해씨");
+							break;
+						}else if(rspn2==2){
+							System.out.println("신중하게 선택해보렴.");
+						}
+					}
+				}
+				Thread.sleep(2000);
+				System.out.println("자, 이제 포켓몬을 얻었으니 여기 로토무를 받으렴");
+				Thread.sleep(1500);
+				System.out.println("System : 로토무 획득!");
+				Thread.sleep(1500);
+				System.out.println("로토무를 통해 메뉴를 선택할 수 있단다. 한번 열어보렴.");
+				Thread.sleep(1500);
+				System.out.println("System : 이제부터 로토무를 열 수 있습니다.");
+				while(true) {
+					System.out.println("9:로토무 열기");
+					int rspn2 = sc.nextInt();
+					if(rspn2 == 9) {
+						Rotomu.openRotomu();
+					}
+					Rotomu.narration();
+				}
+			}
+		}
+	}
+}
